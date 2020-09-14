902b18bfe2354b382bede5aa710b56e0dd39aaea
zpf1452233 <773735496@qq.com>
2020-09-14 10:16:43 +0800

    a

--- dataset.py
+++ dataset.py
@@ -255,7 +255,6 @@ class Yolo_dataset(Dataset):
         truth = {}
         f = open(lable_path, 'r', encoding='utf-8')
         for line in f.readlines():
-            print(line) #dhdhd
             data = line.split(" ")
             truth[data[0]] = []
             for i in data[1:]:
