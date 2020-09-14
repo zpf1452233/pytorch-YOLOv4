9e9952f758f17800cc06be4379a705ced699f3f7
zpf1452233 <773735496@qq.com>
2020-09-14 10:34:29 +0800

    q

--- dataset.py
+++ dataset.py
@@ -272,6 +272,7 @@ class Yolo_dataset(Dataset):
         img_path = self.imgs[index]
         bboxes = np.array(self.truth.get(img_path), dtype=np.float)
         img_path = os.path.join(self.cfg.dataset_dir, img_path)
+        print(img_path)
         use_mixup = self.cfg.mixup
         if random.randint(0, 1):
             use_mixup = 0
